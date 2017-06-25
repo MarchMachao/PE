8101bbf1a158abceb0a2a7da9451b3bdd008bc71
Mr. March <march.machao@gmail.com>
2017-06-25 12:04:56 +0800

    增加部分定时任务的xml

--- src/main/java/cn/vito/coding/check/TimedTask/TimerTaskOnlineDev.java
+++ src/main/java/cn/vito/coding/check/TimedTask/TimerTaskOnlineDev.java
@@ -1,11 +1,7 @@
 package cn.vito.coding.check.TimedTask;
 
-import java.util.Date;
-
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
-import cn.vito.coding.check.utils.StringUtils;
 
 @Component
 public class TimerTaskOnlineDev {
@@ -13,9 +9,40 @@ public class TimerTaskOnlineDev {
 	/**
 	 * 从0秒开始，每隔五秒执行一次
 	 */
-	@Scheduled(cron = "0/5 * * * * ?")
-	public void onlineTask() {
-		System.out.println(StringUtils.dateTimeToString(new Date()));
-	}
+	// @Scheduled(cron = "0/5 * * * * ?")
+	// public void onlineTask() {
+	// System.out.println(StringUtils.dateTimeToString(new Date()));
+	// }
 	
+	/**
+	 * 每年8月25日修改毕业生信息，将a_student.state修改为"已毕业"
+	 */
+	@Scheduled(cron = "0 0 0 25 8 ?")
+	public void updateGraduateState() {
+
+	}
+
+	/**
+	 * 每年8月27日修改请假生信息，将a_student.state修改为"正常"
+	 */
+	@Scheduled(cron = "0 0 0 27 8 ?")
+	public void updateLeaveState() {
+
+	}
+
+	/**
+	 * 每年8月29日修改大三以上的学生的teacher为"学院"
+	 */
+	@Scheduled(cron = "0 0 0 29 8 ?")
+	public void updateStusTeacher() {
+
+	}
+
+	/**
+	 * 每年8月31日为每一位a_student.state为"正常"的学生增加该年体侧任务
+	 */
+	@Scheduled(cron = "0 0 0 31 8 ?")
+	public void insertNewTask() {
+
+	}
 }
